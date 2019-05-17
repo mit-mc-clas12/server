43506ad88aef7483f14007f14b9b64ed6281e673
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-05-17 10:21:45 -0400

    run.sh using gcard ID as argument

--- run.sh
+++ run.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-# Need to pass argumenet from HTCondor to right here (change Gcard = 1 to Gcard = variable_name)
-echo `sqlite3 CLAS12_OCRDB.db "SELECT runscript_text FROM Submissions WHERE GcardID = 1" ` >> runscript.sh
-chmod +x runscript.sh
-./runscript.sh
+gcardID=$1
+
+nodeScript=nodeScript.sh
+
+echo
+echo Downloading runscript with GCARD ID: $gcardID
+echo
+
+rm -f $nodeScript
+sqlite3 ../utils/database/CLAS12_OCRDB.db "SELECT runscript_text FROM Submissions WHERE GcardID = $gcardID"  > $nodeScript
+
+echo $nodeScript downloaded with content:
+echo
+cat $nodeScript
+echo
+echo Now running $nodeScript
+
+chmod +x $nodeScript
+#./$nodeScript
+echo
+echo $nodeScript run completed.
