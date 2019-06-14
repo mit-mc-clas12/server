a9fe5533e7f2b3c0f52e4b02929d4884830af2db
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-06-14 15:14:32 -0400

    fixed runGemc.py

--- submission_files/script_generators/runscript_generators/runGemc.py
+++ submission_files/script_generators/runscript_generators/runGemc.py
@@ -44,17 +44,7 @@ echo
 """
   else:
     runGemc = """
-# Run GEMC
-# --------
-
-# saving date for bookmarking purposes:
-set gemcDate = `date`
-
-echo
-printf "Running {0} events with GEMC using gcard >{2}<"
-echo Executable: `which gemc`
 gemc -USE_GUI=0 -OUTPUT="evio, gemc.evio" -N={0} -INPUT_GEN_FILE="lund, {1}" job.gcard
-cp {2} .
 echo
 printf "GEMC Completed on: "; /bin/date
 echo
