10651c0b84195468b5966ca7578fccd68a17171b
robertej19 <robertej@mit.edu>
2019-07-16 17:05:28 -0400

    "trying to make sqlite work again"

--- run_sqlite.sh
+++ run_sqlite.sh
@@ -11,15 +11,15 @@
 # 3. (optional) lund file
 
 submissionID=$1
-sjob=$2
-lundFile=$3
+sjob = $2
+lundFile = $3
 
 # script name
 nodeScript=nodeScript.sh
 
 outDir="out_"$submissionID"/simu_"$sjob
 mkdir -p $outDir
-cp CLAS12_OCRDB.db $outDir
+cp ../utils/database/CLAS12_OCRDB.db $outDir
 cp *.txt $outDir
 cd $outDir
 
@@ -32,7 +32,7 @@ echo
 rm -f $nodeScript
 
 # sqlite run to download the running script and the gcard. Assuming DB is in the same dir
-sqlite3 CLAS12_OCRDB.db "SELECT runscript_text FROM Submissions WHERE submissionID = $submissionID"  > $nodeScript
+sqlite3 ../utils/database/CLAS12_OCRDB.db "SELECT runscript_text FROM Submissions WHERE submissionID = $submissionID"  > $nodeScript
 echo Now running $nodeScript with submissionID: $submissionID" inside directory: "`pwd`
 
 if [ $# == 3 ]; then
