9aab9111eca4d598849d64fe5d79cdb854146a68
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-10-16 16:20:21 -0400

    just cosmetics

--- src/script_factory.py
+++ src/script_factory.py
@@ -10,8 +10,8 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))+'/../../utils')
 import farm_submission_manager
 import utils, fs, scard_helper, lund_helper, get_args
 
-#Generates a script by appending functions that output strings
-#This function is called by submission_script_manager.py
+# Generates a script by appending functions that output strings
+# This function is called by submission_script_manager.py
 def script_factory(args,script_obj,script_functions,params):
 
   script_text = ""
@@ -20,7 +20,7 @@ def script_factory(args,script_obj,script_functions,params):
   runjob_filename=fs.run_job_obj.file_path+fs.run_job_obj.file_base
   runjob_filename= runjob_filename+ params['file_extension'] + fs.run_job_obj.file_end
 
-  #In the below for loop, we loop through all script_generators for a certain submission script, appending the output of each function to a string
+  # In the below for loop, we loop through all script_generators for a certain submission script, appending the output of each function to a string
   gen_text = [f(params['scard'],
                 username=params['username'],
                 gcard_loc=params['gcard_loc'],
@@ -55,5 +55,5 @@ def script_factory(args,script_obj,script_functions,params):
     
     str_script_db = script_text.replace('"',"'") #I can't figure out a way to write "" into a sqlite field without errors
     # For now, we can replace " with ', which works ok, but IDK how it will run if the scripts were submitted to HTCondor
-    strn = 'UPDATE FarmSubmissions SET {0} = "{1}" WHERE GcardID = {2};'.format(script_obj.file_text_fieldname,str_script_db,params['GcardID'])
+    strn = 'UPDATE FarmSubmissions SET {0} = "{1}" WHERE GcardID = {2};'.format(script_obj.file_text_fieldname, str_script_db, params['GcardID'])
     utils.db_write(strn)
