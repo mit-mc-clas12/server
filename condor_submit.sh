df10fc40b6c08eee996660fc97224ef6c3daa117
David Riser <dmriser@jlab.org>
2019-10-30 09:21:46 -0400

    removing the cred file from otuput

--- condor_submit.sh
+++ condor_submit.sh
@@ -26,5 +26,11 @@ rm -f clas12.condor nodeScript.sh job.gcard
 mysql --defaults-extra-file=msql_conn.txt -N -s --execute="SELECT clas12_condor_text FROM FarmSubmissions WHERE FarmSubmissionID=$submissionID;" | awk '{gsub(/\\n/,"\n")}1' | awk '{gsub(/\\t/,"\t")}1' | sed s/\'\'/\"/g > clas12.condor
 mysql --defaults-extra-file=msql_conn.txt -N -s --execute="SELECT runscript_text FROM FarmSubmissions WHERE FarmSubmissionID=$submissionID;"     | awk '{gsub(/\\n/,"\n")}1' | awk '{gsub(/\\t/,"\t")}1' > nodeScript.sh
 mysql --defaults-extra-file=msql_conn.txt -N -s --execute="SELECT gcard_text FROM Gcards WHERE GcardID=$submissionID;"                           | awk '{gsub(/\\n/,"\n")}1' | awk '{gsub(/\\t/,"\t")}1' > job.gcard
+
+# Get rid of our credentials 
+rm msql_conn.txt 
+
+# Get lund files and send job 
 python lund_downloader.py --url=$lund_url --output_dir='lund_dir'
 condor_submit clas12.condor
+
