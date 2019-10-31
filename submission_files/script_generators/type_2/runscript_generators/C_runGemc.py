af150c05682fd782693c5dffa73ce47661ed7e4d
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2019-10-31 15:18:44 -0400

    quotes happiness part 2

--- submission_files/script_generators/type_2/runscript_generators/C_runGemc.py
+++ submission_files/script_generators/type_2/runscript_generators/C_runGemc.py
@@ -18,9 +18,11 @@ set gemcDate = `date`
 # copying gcard to gemc.gcard
 cp `cat job.gcard` gemc.gcard
 
+set INPUTC=`echo -INPUT_GEN_FILE=\'lund, $lundFile\'`
+
 echo
 echo GEMC executable: `which gemc`
-gemc -USE_GUI=0 -OUTPUT="evio, gemc.evio" -N=0 -INPUT_GEN_FILE=""lund, $lundFile"" gemc.gcard
+gemc -USE_GUI=0 -OUTPUT="evio, gemc.evio" -N=0 $INPUTC gemc.gcard
 echo
 printf "GEMC Completed on: "; /bin/date
 echo
