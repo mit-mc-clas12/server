26ae950441b800df9c20c8358e1d849d9e979068
David Riser <dmriser@jlab.org>
2019-11-05 10:41:43 -0500

    Adding retrieval of options for copying files to F_runScriptFooter.py

--- submission_files/script_generators/type_1/runscript_generators/F_runScriptFooter.py
+++ submission_files/script_generators/type_1/runscript_generators/F_runScriptFooter.py
@@ -3,24 +3,32 @@
 
 def F_runScriptFooter(scard,**kwargs):
 
+        # Try getting these options from the scard, if they don't 
+        # exist, return the default value (yes).
+        generator_out = scard.data.get('generatorOUT', 'yes')
+        gemc_evio_out = scard.data.get('gemcEvioOUT', 'yes')
+        gemc_hipo_out = scard.data.get('gemcHipoOUT', 'yes')
+        reconstruction_out = scard.data.get('reconstructionOUT', 'yes')
+        dst_out = scard.data.get('dstOUT', 'yes')
+
 	strn = """
-#!/bin/csh
+        #!/bin/csh
 
-# Run Script Footer
-# -----------------
-
-set endDate = `date`
-
-echo ==== SubMit-Job === Job Start: $startDate
-echo ==== SubMit-Job === Generator Start: $generatorDate
-echo ==== SubMit-Job === GEMC Start: $gemcDate
-echo ==== SubMit-Job === evio2hipoDate Start: $evio2hipoDate
-echo ==== SubMit-Job === Reconstruction Start: $reconstructionDate
-echo ==== SubMit-Job === Job End: $endDate
-
-# End of Run Script Footer
-# ------------------------
-
-"""
+        # Run Script Footer
+        # -----------------
+        
+        set endDate = `date`
+        
+        echo ==== SubMit-Job === Job Start: $startDate
+        echo ==== SubMit-Job === Generator Start: $generatorDate
+        echo ==== SubMit-Job === GEMC Start: $gemcDate
+        echo ==== SubMit-Job === evio2hipoDate Start: $evio2hipoDate
+        echo ==== SubMit-Job === Reconstruction Start: $reconstructionDate
+        echo ==== SubMit-Job === Job End: $endDate
+        
+        # End of Run Script Footer
+        # ------------------------
+        
+        """
 
 	return strn
