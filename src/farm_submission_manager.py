068a3d6d98e42d751921a22231e3bef83a5e5e25
David Riser <dmriser@jlab.org>
2019-12-18 13:12:27 -0500

    Removed slurm submit

--- src/farm_submission_manager.py
+++ src/farm_submission_manager.py
@@ -19,7 +19,6 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))
                 + '/../submission_files')
 import htcondor_submit
-import slurm_submit
 import update_tables
 import utils
 
@@ -27,17 +26,10 @@ def farm_submission_manager(args, usub_id, file_extension,
                             scard, params, db_conn, sql):
 
   timestamp = utils.gettime() 
-  if scard.data['farm_name'] == "MIT_Tier2" or scard.data['farm_name'] == "OSG":
+  if scard.data['farm_name'] == "OSG":
     utils.printer("Passing to htcondor_submit")
     htcondor_submit.htcondor_submit(args,scard,usub_id,file_extension,params,
                                     db_conn, sql)
-    # update_tables.update_users_statistics(scard, params, timestamp, db_conn, sql)
-
-  elif scard.data['farm_name'] == "JLab":
-    utils.printer("Passing to slurm_submit")
-    slurm_submit.slurm_submit(args,scard,usub_id,file_extension,params)
-    # update_tables.update_users_statistics(scard, params, timestamp, db_conn, sql)
-
   else:
     raise ValueError('Unable to submit for {}'.format(
       scard.data['farm_name']
