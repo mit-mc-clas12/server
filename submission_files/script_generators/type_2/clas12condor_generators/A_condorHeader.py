bd97019e84aac2175cedb2769b59879db0475d2c
ma <maurizio.ungaro@cnu.edu>
2020-05-05 14:16:22 -0400

    changing container pointer name to clas12software ; removing unecessary conditions

--- submission_files/script_generators/type_2/clas12condor_generators/A_condorHeader.py
+++ submission_files/script_generators/type_2/clas12condor_generators/A_condorHeader.py
@@ -4,35 +4,27 @@
 #
 # farm_name possible choices:
 #
-# MIT_Tier2
-# OSG
-# GridPP
+# Note:to submit to MIT_Tier2 add:
+# Requirements  = (GLIDEIN_Site == "MIT_CampusFactory" && BOSCOGroup == "bosco_lns")
 #
 # Note:to bind to CVMFS add:
 # +SingularityBindCVMFS = True
 
-
 def A_condorHeader(scard, **kwargs):
-  farm_name = scard.data.get('farm_name')
 
-  strHeader = """# The SubMit Project: Condor Submission Script
+  strHeader = """# The SubMit Project: Condor Type 2 Submission Script
 # --------------------------------------------
 
 Universe = vanilla
 
 # singularity image and CVMFS binding
-+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/jeffersonlab/clas12simulations:production"
++SingularityImage = "/cvmfs/singularity.opensciencegrid.org/jeffersonlab/clas12software:production"
 """
 
   # OSG Farm Requirements
   requirementsStr = """
 # OSG Requirements
 Requirements = HAS_SINGULARITY == TRUE
-"""
-  # MIT Farm Requirements
-  if farm_name == 'MIT_Tier2':
-	  requirementsStr = """
-Requirements  = (GLIDEIN_Site == "MIT_CampusFactory" && BOSCOGroup == "bosco_lns")
 """
 
   return strHeader + requirementsStr
