2409853d9977bb3c65157407f0c83e0f39478812
robertej19 <robertej@mit.edu>
2020-06-17 19:49:09 -0400

    fixed naming error

--- src/farm_submission_manager_new.py
+++ src/farm_submission_manager_new.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))
                 + '/../../utils')
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))
                 + '/../submission_files')
-import htcondor_submit_new
+import htcondor_submit
 import update_tables
 import utils
 
@@ -26,11 +26,11 @@ def farm_submission_manager(args, usub_id, file_extension,
                             scard, params, db_conn, sql):
 
   timestamp = utils.gettime()
-  if scard.data['farm_name'] == "OSG":
+  if scard.farm_name == "OSG":
     utils.printer("Passing to htcondor_submit")
-    htcondor_submit_new.htcondor_submit(args,scard,usub_id,file_extension,params,
+    htcondor_submit.htcondor_submit(args,scard,usub_id,file_extension,params,
                                     db_conn, sql)
   else:
     raise ValueError('Unable to submit for {}'.format(
-      scard.data['farm_name']
+      scard.farm_name
     ))
