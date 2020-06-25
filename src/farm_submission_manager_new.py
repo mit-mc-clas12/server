aed3c7029dda56c54431374e11cbee3a8210ab84
robertej19 <robertej@mit.edu>
2020-06-24 21:26:17 -0400

    test

--- src/farm_submission_manager_new.py
+++ src/farm_submission_manager_new.py
@@ -14,6 +14,7 @@ import sys
 import time
 from subprocess import PIPE, Popen
 
+
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))
                 + '/../../utils')
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__))
