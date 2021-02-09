622d7c4698d5fdc9f809f565905e06722dbeabcb
maureeungaro <maurizio.ungaro@cnu.edu>
2021-02-09 10:38:19 -0500

    temp removed preferred sites rank

--- submission_files/script_generators/type_2/clas12condor_generators/A_condorHeader.py
+++ submission_files/script_generators/type_2/clas12condor_generators/A_condorHeader.py
@@ -21,7 +21,7 @@ Universe = vanilla
 +SingularityImage = "/cvmfs/singularity.opensciencegrid.org/jeffersonlab/clas12software:{0}"
 +SingularityBindCVMFS = True
 
-Rank = (GLIDEIN_SITE=?='CNAF') || (GLIDEIN_SITE=?='SGridGLA')
+#Rank = (GLIDEIN_SITE=?='CNAF') || (GLIDEIN_SITE=?='SGridGLA')
 
 """.format(scard.submission)
 
