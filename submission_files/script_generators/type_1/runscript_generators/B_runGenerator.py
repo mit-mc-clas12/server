5902d9b1d93d58bee6a2d8243d3470dcf77fa826
maureeungaro <maureeungaro@gmail.com>
2021-04-05 08:30:49 -0400

    removing root files created by generators, if any

--- submission_files/script_generators/type_1/runscript_generators/B_runGenerator.py
+++ submission_files/script_generators/type_1/runscript_generators/B_runGenerator.py
@@ -33,6 +33,9 @@ printf "Running {1} events with generator >{0}< with options: {2} "
 echo
 {0} --trig {1} --docker {2} --seed $seed
 
+# removing any root file that was created
+rm -f *.root
+
 if ($? != 0) then
   echo generator >{0}< failed.
   exit 203
