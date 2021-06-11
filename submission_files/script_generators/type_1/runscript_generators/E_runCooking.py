9c7fb9d4af9092316cc06af6c743d4b13af50538
maureeungaro <maureeungaro@gmail.com>
2021-06-11 09:04:56 -0400

    df commands before and after recon-util

--- submission_files/script_generators/type_1/runscript_generators/E_runCooking.py
+++ submission_files/script_generators/type_1/runscript_generators/E_runCooking.py
@@ -26,7 +26,7 @@ cp /jlab/clas12Tags/$CLAS12TAG"/config/"{0} {0}
 
 set configuration = `echo YAML file: {0}`
 echo
-df -h
+df && df . && df /tmp
 if ($? != 0) then
 	echo df failure
 	echo removing data files and exiting
@@ -43,7 +43,7 @@ if ($? != 0) then
 	rm -f *.hipo *.evio
 	exit 207
 endif
-df -h
+df && df . && df /tmp
 if ($? != 0) then
 	echo df failure
 	echo removing data files and exiting
