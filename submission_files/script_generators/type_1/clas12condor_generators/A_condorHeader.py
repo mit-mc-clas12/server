11e39de9a10f8fb422761f22653881a507b0fcfa
Maurizio Ungaro <ungaro@jlab.org>
2022-09-20 09:52:08 -0400

    removed retries for SGridGLA

--- submission_files/script_generators/type_1/clas12condor_generators/A_condorHeader.py
+++ submission_files/script_generators/type_1/clas12condor_generators/A_condorHeader.py
@@ -22,7 +22,9 @@ Universe = vanilla
 +SingularityBindCVMFS = True
 
 #Rank = (GLIDEIN_SITE=?='CNAF') || (GLIDEIN_SITE=?='SGridGLA')
-Rank = ( (GLIDEIN_SITE=?='CNAF') || (GLIDEIN_SITE=?='SGridGLA') || (GLIDEIN_SITE=?='Lamar-Cluster') ) ? 200 : ((GLIDEIN_SITE=?='SU-ITS') ? 0 : 100)
+#Rank = ( (GLIDEIN_SITE=?='CNAF') || (GLIDEIN_SITE=?='SGridGLA') || (GLIDEIN_SITE=?='Lamar-Cluster') ) ? 200 : ((GLIDEIN_SITE=?='SU-ITS') ? 0 : 100)
+Rank = ( (GLIDEIN_SITE=?='CNAF')  || (GLIDEIN_SITE=?='Lamar-Cluster') ) ? 200 : ((GLIDEIN_SITE=?='SU-ITS') ? 0 : 100)
+MATCH_EXP_JOB_Site != SGridGLA
 
 # Retry automatically
 on_exit_remove   = (ExitBySignal == False) && (ExitCode == 0)
