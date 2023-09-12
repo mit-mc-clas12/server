dfa78a482f7d7184436aca7a47bdb0730a9928dc
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-09-12 13:55:42 -0400

    re-adding palmetto

--- submission_files/script_generators/type_1/clas12condor_generators/A_condorHeader.py
+++ submission_files/script_generators/type_1/clas12condor_generators/A_condorHeader.py
@@ -35,7 +35,7 @@ periodic_release = (NumJobCompletions < 5) && ((CurrentTime - EnteredCurrentStat
 # OSG Requirements
 Requirements = (HAS_SINGULARITY =?= TRUE) && (HAS_CVMFS_oasis_opensciencegrid_org=?=True) && (OSG_HOST_KERNEL_VERSION >= 21700) && (CVMFS_oasis_opensciencegrid_org_REVISION >= 16688) && (OSG_GLIDEIN_VERSION >= 534)
 
-+UNDESIRED_Sites = ""KSU""
++UNDESIRED_Sites = ""KSU,Clemson-Palmetto""
 #+UNDESIRED_Sites = ""KSU,OSG_US_NMSU-Discovery-CE1,Clemson-Palmetto""
 """
 
