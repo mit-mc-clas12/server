26c4d9cdb5adf721c2b7c64cf9814f930e65eb52
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2023-09-12 14:04:17 -0400

    test output redirect

--- submission_files/script_generators/type_1/runscript_generators/C_runGemc.py
+++ submission_files/script_generators/type_1/runscript_generators/C_runGemc.py
@@ -7,6 +7,7 @@ def C_runGemc(scard, **kwargs):
 
 	gemcInputOptions = """ -INPUT_GEN_FILE="lund, {0}" """.format(scard.genOutput)
 	gemcAdditionalOptions = """ -INTEGRATEDRAW="*" """.format(scard.genOutput)
+	redirectOutput = """ | sed '/G4Exception-START/,/G4Exception-END/d'   |  sed '/\*\*\*\*\*\*\*\*\*\*\*\*/,/\*\*\*\*\*\*\*\*\*\*\*\*/d'  """
 
 
 	c12f_home = "/cvmfs/oasis.opensciencegrid.org/jlab/hallb/clas12/soft/noarch/clas12-config/"
@@ -46,9 +47,12 @@ def C_runGemc(scard, **kwargs):
 echo GEMC START:  `date +%s`
 
 echo
-echo GEMC executable: `which gemc`, gcard: {0}
+echo GEMC executable: 
+which gemc
+echo gcard: {0}
+echo
 
-gemc -USE_GUI=0 -N={1} {0} {2} {3} {4} {5} {6} {7} | sed '/G4Exception-START/,/G4Exception-END/d'   |  sed '/\*\*\*\*\*\*\*\*\*\*\*\*/,/\*\*\*\*\*\*\*\*\*\*\*\*/d' 
+gemc -USE_GUI=0 -N={1} {0} {2} {3} {4} {5} {6} {7} {8}
 if ($? != 0) then
 	echo gemc failed
 	echo removing data files and exiting
@@ -76,6 +80,6 @@ echo GEMC END:  `date +%s`
 # End of GEMC
 # -----------
 
-""".format(gcard, scard.nevents, gemcInputOptions, all_vertex_options, torusField, solenField, output, gemcAdditionalOptions)
+""".format(gcard, scard.nevents, gemcInputOptions, all_vertex_options, torusField, solenField, output, gemcAdditionalOptions, redirectOutput)
 
 	return runGemc
