5e74bf2bc3e4ba2787633b1df1d60b09630ff618
Maurizio Ungaro <maureeungaro@gmail.com>
2025-05-09 15:44:09 -0400

    testing double quotes

--- submission_files/script_generators/type_2/runscript_generators/E_runCooking.py
+++ submission_files/script_generators/type_2/runscript_generators/E_runCooking.py
@@ -43,7 +43,7 @@ echo
 setenv MALLOC_ARENA_MAX 1
  
 java -Xmx2560m -Xms1024m -XX:+UseSerialGC \
-    -cp "$CLAS12DIR/lib/clas/*:$CLAS12DIR/lib/services/*:$CLAS12DIR/lib/utils/*" \
+    -cp \"$CLAS12DIR/lib/clas/*:$CLAS12DIR/lib/services/*:$CLAS12DIR/lib/utils/*\" \
     org.jlab.clas.reco.EngineProcessor \
     -y {0} -i {1} -o recon.hipo
 
