0a2442848e9bbaae89f3409c439e9ea9d978f689
Maurizio Ungaro <maureeungaro@gmail.com>
2025-11-03 10:00:58 -0500

    not remving hipo files and checking pelican command

--- submission_files/script_generators/type_1/runscript_generators/F_runScriptFooter.py
+++ submission_files/script_generators/type_1/runscript_generators/F_runScriptFooter.py
@@ -59,13 +59,22 @@ echo Running pelican on: $outputFileName
 # running pelican
 echo pelicon output to osdf:///jlab-osdf/clas12/volatile/osg/{2}/$submissionID/$outputFileName
 /usr/bin/pelican -d object put $outputFileName osdf:///jlab-osdf/clas12/volatile/osg/{2}/$submissionID/$outputFileName
+if ($? != 0) then
+	echo pelican failure
+	echo removing data files and exiting
+    rm -f *.hipo *.evio *.sqlite
+	exit 211
+endif
+
 echo Additional cleanup
 rm -f core* *.gcard
 rm -f recon.hipo gemc.hipo gemc.merged.hipo gemc_denoised.hipo 
 rm -f run.sh nodeScript.sh condor_exec.exe
 rm -f RNDMSTATUS random-seeds.txt {1}
 rm -f gemc.evio
-rm -f *.hipo 
+
+# cleaning up so no hipo comes back
+#rm -f *.hipo 
 
 echo
 echo nodeScript run completed.
--- submission_files/script_generators/type_2/runscript_generators/F_runScriptFooter.py
+++ submission_files/script_generators/type_2/runscript_generators/F_runScriptFooter.py
@@ -60,6 +60,12 @@ echo Running pelican on: $outputFileName
 # running pelican
 echo pelicon output to osdf:///jlab-osdf/clas12/volatile/osg/{2}/$submissionID/$outputFileName
 /usr/bin/pelican -d object put $outputFileName osdf:///jlab-osdf/clas12/volatile/osg/{2}/$submissionID/$outputFileName
+if ($? != 0) then
+	echo pelican failure
+	echo removing data files and exiting
+    rm -f *.hipo *.evio *.sqlite
+	exit 211
+endif
 
 echo Additional cleanup 
 rm -f core* *.gcard
@@ -67,7 +73,9 @@ rm -f recon.hipo gemc.hipo gemc.merged.hipo gemc_denoised.hipo
 rm -f run.sh nodeScript.sh condor_exec.exe
 rm -f RNDMSTATUS random-seeds.txt {1}
 rm -f gemc.evio
-rm -f *.hipo 
+
+# cleaning up so no hipo comes back
+#rm -f *.hipo 
 
 echo
 echo nodeScript run completed.
